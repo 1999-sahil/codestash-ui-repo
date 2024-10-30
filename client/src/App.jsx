a2488a156b8ae38e47d12a0af90cd53c73d3aaaa
Sahil Ahmed <sahilahmed466@gmail.com>
2024-10-31 00:56:56 +0530

    Setting up configurations for client/server side

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import './App.css'
+import { ThemeContext } from './context/theme-context';
+import ThemeToggle from './components/theme-toggle';
+
+function App() {
+  const { darkTheme } = useContext(ThemeContext);
+  return (
+    <>
+      <div className={`${darkTheme ? "dark-theme" : "light-theme"} h-screen`}>
+      <header className="App-header">
+        <h1>Theme Switching App</h1>
+        <ThemeToggle />
+      </header>
+      <div className="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis
+        ut orci nec sodales. Donec dignissim tortor eget metus mollis, vel
+        gravida lectus accumsan. Vestibulum dapibus tortor in massa lacinia
+        tempus. Morbi et dolor et ipsum eleifend viverra in a erat. Nam vel
+        rhoncus urna, eu finibus justo. Nunc sodales lacinia turpis, vitae
+        viverra erat rutrum eget. Phasellus dapibus imperdiet odio, consectetur
+        rutrum velit pharetra elementum. Aliquam tempor mauris nec consequat
+        cursus. Nullam mi libero, sodales ut ornare at, lobortis ut metus. Cras
+        quis ex non odio vehicula elementum et in magna. Phasellus eget
+        ullamcorper orci. Duis in massa accumsan, vehicula arcu sit amet,
+        pulvinar mi. Cras massa lorem, facilisis a arcu eu, mollis convallis
+        urna. Aliquam semper tristique nunc vel placerat. Etiam nec magna ex{" "}
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+        mukta: ["Mukta", "sans-serif"],
+        ubuntu: ["Ubuntu Condensed", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        kanit: ["Kanit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
